d0bec98863662eb6fe9ec9b5365728825f9a130e
Kevin dan mathew <lee777601@gmail.com>
2026-03-15 02:40:24 +0530

    Update syscall_arch.h

--- arch/x86_64/syscall_arch.h
+++ arch/x86_64/syscall_arch.h
@@ -1,12 +1,11 @@
 #ifndef SYSCALL_ARCH_H
 #define SYSCALL_ARCH_H
 
-#define __SYSCALL_LL_E(x) (x)
-#define __SYSCALL_LL_O(x) (x)
-
-// Inline functions for syscalls (0-6 arguments) using 'syscall' instruction
-// See musl implementation for full asm details
-static inline long __syscall0(long n) { /* ... */ }
-static inline long __syscall1(long n, long a1) { /* ... */ }
-// ... (implementations for 2-6 arguments)
+// Core inline assembly for x86_64 syscalls (0-6 arguments)
+static __inline long __syscall0(long n) {
+	unsigned long ret;
+	__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n) : "rcx", "r11", "memory");
+	return ret;
+}
+// ... (Implementation for __syscall1 through __syscall6 omitted for brevity)
 #endif
